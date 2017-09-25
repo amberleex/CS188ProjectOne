0bc3c62067a623142baef55397243b6136c9c1ea
amberleex <amber.smith@drake.edu>
2017-09-25 11:30:07 -0500

    Revert "tutorial 3 with error for Professor Brady To Check"
    This reverts commit 73c665178bdebf02e1db02c1742aa51f667822cf.

--- app/src/main/java/com/amberleesmith/beautifulbulldog/LoginActivity.java
+++ app/src/main/java/com/amberleesmith/beautifulbulldog/LoginActivity.java
@@ -49,7 +49,7 @@ public class LoginActivity extends AppCompatActivity {
         SyncUser.loginAsync(myCredentials, "http://52.205.194.154:9080", new SyncUser.Callback() {
             @Override
             public void onSuccess(SyncUser user) {
-                SyncConfiguration configuration = new SyncConfiguration.Builder(user, "realm://52.205.194.154:9080/~/bulldog").disableSSLVerification().waitForInitialRemoteData().schemaVersion((long) 12.0).build();
+                SyncConfiguration configuration = new SyncConfiguration.Builder(user, "realm:/52.205.194.154:9080/~/bulldog").disableSSLVerification().waitForInitialRemoteData().schemaVersion((long) 12.0).build();
                 Realm.setDefaultConfiguration(configuration);
 
                 Realm.getInstanceAsync(configuration, new Realm.Callback() {
